bd63ef4cc2c865f076f80d28942702c9011eb1e1
liapetelko <98822243+liapetelko@users.noreply.github.com>
2022-02-03 13:01:22 +0200

    Update try.py

--- try.py
+++ try.py
@@ -2,5 +2,10 @@ import streamlit as st
 
 st.title("Hi")
 st.header("Hello!")
+st.subheader("subb")
+st.text("please write the vugrapg name")
+if st.button("press this"):
+  st.text("Write something")
+image = st.file_uploader("Upload image",[JPG,PNG])
 
-st.button("press this")
+#st.sidebar.title("select..")
