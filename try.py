721f18b0e66752b77b8285bf85950f3b266e7717
liapetelko <98822243+liapetelko@users.noreply.github.com>
2022-02-03 16:15:08 +0200

    Update try.py

--- try.py
+++ try.py
@@ -6,6 +6,6 @@ st.subheader("subb")
 st.text("please write the vugrapg name")
 if st.button("press this"):
   st.text("Write something")
-image = st.file_uploader("Upload image",[JPG,PNG])
+#image = st.file_uploader("Upload image",[JPG,PNG])
 
 #st.sidebar.title("select..")
