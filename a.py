7ebb5fb27a620e8ceaa94ea3260b5001cf922943
liapetelko <98822243+liapetelko@users.noreply.github.com>
2022-02-03 19:14:25 +0200

    Add files via upload

---
+++ a.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Hi")
+st.header("Hello!")
+st.subheader("subb")
+st.text("please write the vugrapg name")
+if st.button("press this"):
+  st.text("Write something")
+  #selectbox = st.selectbox(["Yes","No"])
+  #if st.download_button()=="hello":
+st.button("works!")
+#image = st.file_uploader("Upload image",[JPG,PNG])
+
+#st.sidebar.title("select..")
